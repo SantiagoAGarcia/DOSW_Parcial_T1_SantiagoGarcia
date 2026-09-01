98822bdedd7fba2c7650ca2f59270466c41a2f4a
SantiagoAGarcia <90394035+SantiagoAGarcia@users.noreply.github.com>
2026-09-01 11:43:44 -0500

    Add files via upload

---
+++ src/test/java/edu/dosw/parcial/AppTest.java
@@ -0,0 +1,15 @@
+package edu.dosw.parcial;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Pruebas unitarias básicas para App.
+ */
+public class AppTest {
+
+    @Test
+    public void appShouldRunWithoutErrors() {
+        assertTrue(true);
+    }
+}
